061da325c984689041d15ac3a0f877eaba8e0721
NaxonRO <andrei@andrexnet.com>
2024-09-30 22:59:39 +0300

    Field added

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+# For graphical interface
+import tkinter as tk
+
+#Field text for the show of number and operation
+field_text = "1"
+def add_to_field(sth):
+    global field_text
+    field_text = field_text+str(sth)
+
+window = tk.Tk()
+window.geometry("300x300")
+field = tk.Text(window,height=2,width=21,font=("Times New Roman", 20))
+field.grid(row = 1, column = 1, columnspan = 4)
+window.mainloop()
